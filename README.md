f19253f656e57984cf8c5795e48d74e7835ace1e
Pontus Nyman <pontusnyman@gmail.com>
2018-05-24 00:20:58 +0200

    First draft.

---
+++ README.md
@@ -0,0 +1,143 @@
+# txt2pdf
+
+Convert one or more text files to a PDF document using LaTeX typesetting.
+
+This tool was developed for typesetting novels and short story collections written in pure text files.
+The goal is to keep the text files as clean as possible with a minimum of structured formatting, i.e. a harsh subset of Markdown.
+
+Most people writing fiction and literature use Word, Pages, Scrivener or some other WYSIWYG editor.
+However, there are many reasons to use a plain text editor, e.g. better text editing, less distraction of text formatting and productivity features such as full screen and zen modes (as found in [Visual Studio Code](https://code.visualstudio.com/) and [Sublime Text](https://www.sublimetext.com/)).
+
+## Prerequisites
+
+* Python 3
+  * jinja2
+  * langdetect
+* [MikTex](https://miktex.org/)
+
+## Installation
+
+1. Clone or download this project.
+
+2. Create an isolated Python environment using **virtualenv** or just use the system installation and make sure the required packages are installed using the **requirement.txt** file.
+
+        python -m pip install -r requirement.txt
+
+3. Run the following command to read more about the command line arguments:
+
+        python txt2pdf.py --help
+
+When running the script for the first time (with real and valid inputs) MikTex will install multiple LaTeX packages. You should accept this.
+
+## Example
+
+Let's say you have written The Great Novel. It consists of ten chapters in ten separate text files. All the files are located in the same directory among other useful resources for the writing project.
+
+An example directory tree could look like this:
+
+```text
+TheGreatNovel
+│   chapter01.txt
+│   chapter02.txt
+│   chapter03.txt
+│   chapter04.txt
+│   chapter05.txt
+│   chapter06.txt
+│   chapter07.txt
+│   chapter08.txt
+│   chapter09.txt
+│   chapter10.txt
+│
+└───fragments
+│   │   ...
+│
+└───old
+│   │   ...
+│
+└───research
+│   │   ...
+│   │
+│   └───maps
+│       │   ...
+│
+└───build
+    │   generate_book.bat
+    │   the_great_novel.pdf
+    │
+    └───env
+    |   │   ...
+    │
+    └───txt2pdf
+        │   ...
+
+```
+
+A recommendation is to create a directory (called _build_ in the example above) with a batch or bash script that creates the PDF document. If you have a big writing project with lots of chapters or short stories (i.e. text files) the list of command arguments can be quite long. You don't want to type it every time...
+
+Besides, using a script makes it easy and fast to generate the PDF document and you want to be agile also when you write The Great Novel. Right?
+
+The **generate_book.bat** could look something like this:
+
+```batch
+@echo off
+env\Scripts\python txt2pdf\txt2pdf.py ^
+--title The Great Novel ^
+--author John Doe ^
+--output the_great_novel ^
+chapter01.txt ^
+chapter02.txt ^
+chapter03.txt ^
+chapter04.txt ^
+chapter05.txt ^
+chapter06.txt ^
+chapter07.txt ^
+chapter08.txt ^
+chapter09.txt ^
+chapter10.txt ^
+```
+
+## Text file format
+
+The input text files to txt2pdf are plain text files. You shouldn't need to worry about formatting. There are just a few "features" to remember.
+
+The first line of the text file represent the chapter name (or the name of the short story). E.g. **chapter01.txt** in the example above could look like this:
+
+```text
+Chapter one
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+```
+
+Minus characters in the text are converted to dashes when appropriate.
+
+### Markdown subset
+
+A very small subset of Markdown syntax is supported (i.e. only what I, the author of txt2pdf, so far have needed...).
+
+Quotes can be written like this to get a nice format in the output:
+
+```text
+The next day a read the following in the newspaper:
+
+> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+
+I couldn't believe my eyes.
+```
+
+Verses can be written with with four spaces. Line breaks will then be kept. E.g.:
+
+```text
+Suddenly she started to recite a poem:
+
+    Lorem ipsum dolor sit amet
+    consectetur adipiscing elit,
+    sed do eiusmod tempor incididunt
+    ut labore et dolore magna aliqua
+
+I couldn't believe my ears.
+```
+
+## Output typesetting
+
+Since LaTeX is used in the background for the actual typesetting, people familiar with LaTeX will recognize the typesetting of the output.
+I have used the document classes that I think look good. If someone wants to change the look of the PDF output [template.tex](./template.tex) is a good start.
---
+++ txt2pdf.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""txt2pdf - Convert one or more text files to a PDF document using LaTeX typesetting.
+
+This tool was developed for typesetting novels and short story collections written in pure text files.
+The goal is to keep the text files as clean as possible with a minimum of structured formatting, i.e. a harsh subset of Markdown.
+"""
+
+import os
+import sys
+import argparse
+import subprocess
+import tempfile
+import shutil
+import glob
+import re
+
+import jinja2
+import langdetect
+
+
+# Exception classes.
+
+class InvalidInputTxtFileException(Exception):
+    def __init__(self, filename=None):
+        self.filename = filename
+
+    def __str__(self):
+        if self.filename is not None:
+            return self.filename
+
+class ErrorWhenExecutingLatexException(Exception):
+    def __init__(self, completed_process=None):
+        self.completed_process = completed_process
+
+    def __str__(self):
+        if self.completed_process is not None:
+            return self.completed_process.stderr
+
+
+def convert_simplified_markdown_to_latex(content):
+
+    # Pre-process text to make it properly LaTeX formatted
+    content = content.replace('"', "''")            # proper citation marks
+    content = content.replace(' - ', ' -- ')        # proper LaTeX dash
+    content = content.replace('&', r'\&')           # proper '&' character
+    content = content.replace('#', r'\#')           # proper '#' character
+    content = re.sub(r'\[\[.*?\]\]', '', content)   # Remove text between [[ ... ]] delimiters
+    content = re.sub(r"\n- ", r"\n-- ", content)    # replace dash at the beginning of line
+
+    paragraphs = content.splitlines()
+    title = paragraphs[0]
+    paragraphs = paragraphs[1:]
+    new_paragraphs = []
+
+    # Pre-process various quotes
+    verse = False
+    quote = False
+    for paragraph in paragraphs:
+        if paragraph[0:4] == "    ":
+            if not verse:
+                # Start of verse
+                new_paragraphs.append('\\begin{verse}')
+                new_paragraphs.append(paragraph.lstrip() + '\\\\')
+                verse = True
+                continue
+            else:
+                # After first line of verse
+                new_paragraphs.append(paragraph.lstrip() + '\\\\')
+                continue
+        elif verse:
+            # End of verse
+            new_paragraphs.append('\\end{verse}')
+            verse = False
+        if paragraph[0:2] == "> ":
+            if not quote:
+                # Start of quote
+                new_paragraphs.append('\\begin{quote}')
+                new_paragraphs.append(paragraph[2:])
+                quote = True
+                continue
+            else:
+                # After first line of quote
+                new_paragraphs.append('\\\\' + paragraph[2:])
+                continue
+        elif quote:
+            # End of quote
+            new_paragraphs.append('\\end{quote}')
+            quote = False
+        if paragraph == "*":
+            new_paragraphs.append('\\vspace{6mm}')
+        else:
+            new_paragraphs.append(paragraph)
+    content = "\n".join(new_paragraphs)
+
+    return (title, content)
+
+
+def preprocess_input(args):
+    metadata = {}
+    metadata['title'] = ' '.join(args.title)
+    metadata['author'] = ' '.join(args.author)
+    metadata['multiple_chapters'] = len(args.sources) > 1
+    metadata['wide_line_spacing'] = args.wide_line_spacing
+    metadata['chapters'] = []
+
+    lang_detect_input = None
+
+    # Process inputs
+    if args.basepath is not None:
+        basepath = args.basepath
+    else:
+        basepath = '../'
+
+    for path in args.sources:
+        path = os.path.join(basepath, path)
+        try:
+            with open(path, 'rU') as f:
+                content = f.read()
+        except IOError:
+            raise InvalidInputTxtFileException(path)
+
+        if lang_detect_input is None:
+            # Use content from first input to detect language.
+            lang_detect_input = content
+
+        (chapter_title, chapter_content) = convert_simplified_markdown_to_latex(content)
+        chapter_metadata = {'title': chapter_title, 'content': chapter_content}
+        metadata['chapters'].append(chapter_metadata)
+
+    # Detect language
+    detected_language = langdetect.detect(lang_detect_input)
+    language_dict = {
+        'sv': 'swedish',
+        'en': 'english'
+    }
+    if detected_language in language_dict:
+        metadata['language'] = language_dict[detected_language]
+    else:
+        print("Add support for more languages!", file=sys.stderr)
+        metadata['language'] = None
+
+    return metadata
+
+
+def find_latex_binary():
+    # Only supports Windows for now. This should be the only code that needs to be fixed to support Linux.
+    for filename in glob.iglob("c:/Program Files/**/pdflatex.exe", recursive=True):
+        return filename
+    for filename in glob.iglob("c:/Program Files (x86)/**/pdflatex.exe", recursive=True):
+        return filename
+    home = os.path.expanduser("~")
+    for filename in glob.iglob(home + "/**/pdflatex.exe", recursive=True):
+        return filename
+    return None
+
+
+def generate_latex_source(metadata, tex_path):
+    template_dir = os.path.dirname(os.path.realpath(__file__))
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True)
+    tex_template = env.get_template('template.tex') 
+    with open(tex_path, 'w') as tex_file:
+        tex_file.write(tex_template.render(metadata))
+
+
+def generate_pdf_output(latex_bin, tex_path, temp_dir):
+    result = subprocess.run([latex_bin, tex_path, "-quiet", "-halt-on-error", "-output-directory", temp_dir], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.returncode != 0:
+        raise ErrorWhenExecutingLatexException(result)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a PDF out of one or more text files.')
+    parser.add_argument('sources', metavar='path', type=str, nargs='+', help='text file(s)')
+    parser.add_argument('--title', nargs='+', help='Document title.')
+    parser.add_argument('--author', nargs='+', help='Document author.')
+    parser.add_argument('--basepath', help='Base path of text file(s).')
+    parser.add_argument('--output', default='nameless', help='Output filename.')
+    parser.add_argument('--wide-line-spacing', action='store_true', help='Extra wide line spacing.')
+    args = parser.parse_args()
+
+    try:
+        print("Looking for LaTeX binaries on your system...")
+        latex_bin = find_latex_binary()
+        if latex_bin is None:
+            print("Could not find LaTeX on your system!")
+            sys.exit(1)
+
+        print("Processing input...")
+        metadata = preprocess_input(args)
+
+        temp_dir = tempfile.mkdtemp()
+        tex_path = os.path.join(temp_dir, args.output + ".tex")
+        output_filename = args.output + ".pdf"
+        pdf_path = os.path.join(temp_dir, output_filename)
+
+        print("Generating intermediate files...")
+        generate_latex_source(metadata, tex_path)
+        print("Generating PDF '{}'...".format(output_filename))
+        generate_pdf_output(latex_bin, tex_path, temp_dir)
+        shutil.copy(pdf_path, "./")
+
+        print("Cleaning up intermediate files...")
+        shutil.rmtree(temp_dir)
+
+    except InvalidInputTxtFileException as e:
+        print("Couldn't find input file '{}'!".format(e.filename), file=sys.stderr)
+        sys.exit(1)
+
+    except ErrorWhenExecutingLatexException as e:
+        print("Something bad happened during LaTeX execution!\n")
+        if e.completed_process.stderr is not None:
+            print(e.completed_process.stderr)
+        sys.exit(1)
+
+    #except:
+    #    print("Unexpected error: {}".format(sys.exc_info()[0]))
+    #    print(str(sys.exc_info()[2]))
